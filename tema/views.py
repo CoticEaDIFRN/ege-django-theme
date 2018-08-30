0f8b2a4151235f1b2b86bb3a8c07bb71386d2b6c
Luiz Antonio <luiz@ead207313p302.local>
2018-08-30 14:17:33 -0300

    [UPD] - Adicionando Tema - 1ª versão

---
+++ tema/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Input
+from django.template import loader
+
+
+def index(request):
+    input = Input.objects.all()
+    template = loader.get_template('index.html')
+    context = {
+        'input': input,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def form(request):
+    input = Input.objects.all()
+    template = loader.get_template('form.html')
+    context = {
+        'input': input,
+    }
+    return HttpResponse(template.render(context, request))
