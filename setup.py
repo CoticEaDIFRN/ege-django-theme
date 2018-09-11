db51b224e2d1f1006dc36a0fed165a6fa54e68d8
Luiz Antonio <luizvpc@gmail.com>
2018-09-11 18:05:47 -0300

    Release 0.2

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
     name="ege-django-theme",
-    version="1",
-    author="Luiz Antonio",
+    package_data={'ege-django-theme': [
+        'ege-django-theme/*',
+        'ege-django-theme/migrations/*',
+        'ege-django-theme/templates/*'
+    ],},
+    version="0.2",
+    author="Luiz Antonio Freitas de Assis",
     author_email="luizvpc@gmail.com",
     description="EGE Django Theme",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/CoticEaDIFRN/ege-django-theme",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
+    ]
 )
