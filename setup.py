5bafea74991ce20679a3da207d0781aca69ac9cd
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2018-09-12 16:09:25 -0300

    Release 0.2.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 import setuptools    
 from distutils.core import setup
 setup(
-    name="ege-django-theme",
-    package_data={'ege-django-theme': [
-        'ege-django-theme/*',
-        'ege-django-theme/migrations/*',
-        'ege-django-theme/templates/*'
+    name="ege_django_theme",
+    package_data={'ege_django_theme': [
+        'ege_django_theme/*',
+        'ege_django_theme/migrations/*',
+        'ege_django_theme/templates/*'
     ],},
-    version="0.2",
+    version="0.2.1",
     author="Luiz Antonio Freitas de Assis",
     author_email="luizvpc@gmail.com",
     description="EGE Django Theme",
