54e7848f763202c25baf23acad8c77f86ad2a511
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2018-09-12 16:14:02 -0300

    Release 0.2.1

--- setup.py
+++ setup.py
@@ -3,17 +3,13 @@ import setuptools
 from distutils.core import setup
 setup(
     name="ege_django_theme",
-    package_data={'ege_django_theme': [
-        'ege_django_theme/*',
-        'ege_django_theme/migrations/*',
-        'ege_django_theme/templates/*'
-    ],},
+    packages=setuptools.find_packages(),
+    include_package_data=True,
     version="0.2.1",
     author="Luiz Antonio Freitas de Assis",
     author_email="luizvpc@gmail.com",
     description="EGE Django Theme",
     url="https://github.com/CoticEaDIFRN/ege-django-theme",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
