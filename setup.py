085601af18aca0f3b799029ccbacb60c0058e28a
Luiz <luizvpc@gmail.com>
2018-10-15 15:38:09 -0300

    Release 0.3.2

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     author="Luiz Antonio Freitas de Assis",
     author_email="luizvpc@gmail.com",
     url="https://github.com/CoticEaDIFRN/ege-django-theme",
-    packages=['ege_django_theme', 'ege_django_theme/migrations', 'ege_django_theme/templates'],
+    packages=['ege_django_theme', 'ege_django_theme/migrations', 'ege_django_theme/static', 'ege_django_theme/templates', 'ege_django_theme/templatetags'],
     include_package_data=True,
     # install_requires=[],
-    version="0.3.1",
+    version="0.3.2",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
