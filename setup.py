37cf8b46361b2e1de3aab51322d73ea3c3330e07
Luiz <luizvpc@gmail.com>
2018-10-16 12:05:11 -0300

    Release 0.3.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=['ege_django_theme', 'ege_django_theme/migrations', 'ege_django_theme/static', 'ege_django_theme/templates', 'ege_django_theme/templatetags'],
     include_package_data=True,
     # install_requires=[],
-    version="0.3.3",
+    version="0.3.4",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
