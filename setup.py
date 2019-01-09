bc81a7b73192889f9eaa8a1fe5440aba31a05bb9
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2019-01-09 19:06:01 -0300

    Release 0.5.0

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 setup(
-    name='ege_django_theme',
-    description='EGE Django Theme',
-    long_description='EGE Django Theme',
+    name='ege_theme',
+    description='EGE Theme',
+    long_description='EGE Theme',
     license='MIT',
     author='Luiz Antonio Freitas de Assis',
     author_email='luizvpc@gmail.com',
-    packages=['ege_django_theme', 'ege_django_theme/migrations', 'ege_django_theme/static', 'ege_django_theme/templates', 'ege_django_theme/templatetags'],
+    packages=['ege_theme', 'ege_theme/migrations', 'ege_theme/static', 'ege_theme/templates', 'ege_theme/templatetags'],
     include_package_data=True,
-    version='0.4.8',
-    download_url='https://github.com/CoticEaDIFRN/ege_django_theme/releases/tag/0.4.8',
-    url='https://github.com/CoticEaDIFRN/ege_django_theme',
+    version='0.5.0',
+    download_url='https://github.com/CoticEaDIFRN/ege_theme/releases/tag/0.5.0',
+    url='https://github.com/CoticEaDIFRN/ege_theme',
     keywords=['EGE', 'JWT', 'Django', 'Auth', 'SSO', 'client', ],
     # install_requires=['PyJWT==1.7.1', 'requests==2.21.0', 'django>=2.0,<3.0'],
     classifiers=[
